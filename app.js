2da42d2a77db69067af753a08feb5338d7cf50cb
Jean Carlo Sinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-11-18 06:37:53 -0500

    will Kill this project

--- app.js
+++ app.js
@@ -2,16 +2,54 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js");
-
+const mongoose = require('mongoose');
 // Express Use
 const app = express();
 
+// MongoDB URI
+const userName = 'jeancsinisterra';
+const password = 'Nova25';
+const uri = `mongodb+srv://${user}:${password}@cluster0.x3evpni.mongodb.net/?retryWrites=true&w=majority`;
+
+// Mongoose Connection
+
+mongoose.connect(uri, 
+{ useNewUrlParser: true , useUnifiedTopology: true})
+.then(() => console.log("DataBase Connected succesfully"))
+.catch(e => console.log(e));
+
+// Mongoose Schema
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "ToDo List for today"
+});
+
+const item2 = new Item({
+    name: "Hit the button to add a new task"
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err){
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Succesfuly saved default Items to database");
+    }
+})
+
 // PORT
 const PORT = process.env.PORT || 3000;
-
-// Array
-const items = ["Add below your new task"];
 
 // EJS Template Engine setup
 app.set("view engine", "ejs");
@@ -24,8 +62,7 @@ app.use(express.static(__dirname + "/public"));
 
 // Route Setup
 app.get("/", function(req, res){    
-    let day = date.getDate();
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {kindOfDay: "Today", newListItems: items});
 });
 
 // Request of Text Item from EJS file
@@ -37,7 +74,22 @@ app.post("/", function(req, res){
     res.redirect("/");
 })
 
+// connectDB();
+
+
+// MongoDB URI entry point
+// const uri = "mongodb+srv://jeancsinisterra:Pulsar2594@cluster0.x3evpni.mongodb.net/?retryWrites=true&w=majority";
+
+// async function connectDB(){
+//     try {
+//         await mongoose.connectDB(uri)
+//         console.log("Connected to MongoDB")
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }   
+
 // Running Server
-app.listen(PORT, function(){
+app.listen(PORT, function() {
     console.log(`Server is running on port: ${PORT} `)
 })
